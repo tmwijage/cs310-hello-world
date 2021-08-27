5f83d6f5ab080c6c75e0cc2a13995c9a5616f95f
Tajuddin Mwijage <idrisatajuddin@gmail.com>
2021-08-27 12:05:27 -0500

    Initial commit

--- Class1.java
+++ Class1.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Tajuddin Mwijage
  */
 
 public class Class1 {
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,13 @@
 /**
  *
- * @author Your Name
+ * @author Tajuddin Mwijage
  */
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Class c1 = new Class1();
-        Class c2 = new Class2();
+        Class1 = new Class1();
+        Class2 = new Class2();
         System.out.println("Hello, World!");
     }
     private void f1() {
